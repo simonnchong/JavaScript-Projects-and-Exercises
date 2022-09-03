884e98d86dcd132aafc2437f756e3e4ceea3060a
Simon <simonnchong@gmail.com>
2022-09-04 00:20:00 +0800

    upload all files

---
+++ basic_syntax/contitional.js
@@ -0,0 +1,34 @@
+/*
+
+== 
+check for equality of value
+
+=== 
+check for equality of value and datatype
+
+< less than
+
+> 
+greater than
+
+<= 
+less than and equal to
+
+>= 
+greater than and equal to
+
+!= 
+not equal to (value)
+
+!== 
+not equal too (value and datatype)
+
+
+*/
+
+let weather = prompt("How is the weather? rain/hot");
+if (weather == "rain") {
+    console.log("Grab your umbrella ☔");
+} else {
+    console.log("Wear your sunglasses 😎");
+}
---
+++ basic_syntax/printing.js
@@ -0,0 +1,13 @@
+// ---------------------------------------------------- PRINT OUTPUT -------------------------------------------------------
+console.log("Hello World");
+alert("Hello World");
+
+// this is a comment line
+/* This is a 
+        multiple lines 
+        comments */
+var my_name = "Simon Chong";
+console.log(my_name);
+
+sentence = "Welcome to javascript world!";
+console.log(`Hello ${my_name}, ${sentence}`); // backtick is a special quote to format the string
